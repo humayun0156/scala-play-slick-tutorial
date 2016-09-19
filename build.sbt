36fefce971c52e9f6a5a903a4bbc039ac89a86b4
Md. Humayun Kabir <humayun0156@gmail.com>
2016-09-19 22:47:05 +0600

    build.sbt & MainBuild.scala

--- build.sbt
+++ build.sbt
@@ -3,8 +3,15 @@ name := """scala-tutorial"""
 version := "1.0"
 
 scalaVersion := "2.11.8"
+lazy val akkaVersion = "2.4.9"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion
+)
+
+// append options passed to the Scala compiler
+scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature")
 
 
 fork in run := true
---
+++ project/MainBuild.scala
@@ -0,0 +1,23 @@
+import sbt._
+
+object MainBuild extends Build {
+  //scala-in action
+  val scalainaction = Project("scalainaction", file("scalainaction"))
+  val scinaction_ch01 = Project("scalainaction-ch01", file("scalainaction/ch01"))
+  val scinaction_ch02 = Project("scalainaction-ch02", file("scalainaction/ch02"))
+  val scinaction_ch03 = Project("scalainaction-ch03", file("scalainaction/ch03"))
+  val scinaction_ch03_mongo = Project("scalainaction-ch03-scala-mongo-driver", file("scalainaction/ch03/scala-mongo-driver"))
+  val scinaction_ch04 = Project("scalainaction-ch04", file("scalainaction/ch04"))
+  val scinaction_ch05 = Project("scalainaction-ch05", file("scalainaction/ch05"))
+
+  val main = Project("scala-tutorial", file("."))
+      .aggregate(
+        scalainaction,
+        scinaction_ch01,
+        scinaction_ch02,
+        scinaction_ch03,
+        scinaction_ch03_mongo,
+        scinaction_ch04,
+        scinaction_ch05
+  )
+}
